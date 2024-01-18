8639eafeb7c1d27653421de00c5e36154346b2bb
Saurav-Suman-TM <saurav@teachmint.com>
2024-01-18 21:15:21 +0530

    initial commit

---
+++ app/src/main/java/com/example/weatherapp/landing/LandingActivity.kt
@@ -0,0 +1,39 @@
+package com.example.weatherapp.landing
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.viewModels
+import androidx.lifecycle.MutableLiveData
+import com.example.weatherapp.R
+import com.example.weatherapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LandingActivity : AppCompatActivity() {
+    private val viewModel: LandingActivityViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        bindViews()
+    }
+
+    override fun onResume() {
+        viewModel.getCurrentWeatherInfo()
+        viewModel.getForecastInfo()
+        super.onResume()
+    }
+
+    private fun bindViews() {
+        viewModel.imageListLiveData.observe(this) {
+            binding.tvMainTemp.text = it
+        }
+    }
+}
